57786ce50a26f4c8f59f32bab5f16611c7a118c1
kpzinnm <gabriel.santos.alves@ccc.ufcg.edu.br>
2023-11-28 16:18:29 -0300

    Maze, A* search

--- search/maze/Maze*.py
+++ search/maze/Maze*.py
@@ -1,15 +1,24 @@
+"""
+Vamos agora fazer a implementação do algoritmo A* search, aplicando no contexto no Maze
+A modificação que faremos é a implementação de uma função heurística que leva em consideração
+a distância do estado atual até o estado destino e a quantidade de passos que ele precisou para chegar
+até o estado atual. Com essa estrategia, vamos tentar otimizar o processo da busca pelo melhor caminho.
+"""
 class Node():
-    def __init__(self, state, action, parent):
+    def __init__(self, state, action, parent, distance, steps):
         self.state = state
         self.action = action
         self.parent = parent
+        self.distance = distance
+        self.steps = steps
+        self.heuristic = distance + steps
 
     def __eq__(self, other):
         if(isinstance(other, Node)):
             return self.state == other.state
         return False
 
-class Maze():   
+class Maze():
     def __init__(self, filename):
         with open(filename,'r') as arquivo:
             conteudo = arquivo.read()
@@ -19,15 +28,15 @@ class Maze():
         self.width = len(self.conteudo[0])
         self.finalState = (0,0)
         self.initialState = (0,0)  
-    
-    # Define o ponto inicial e final
+       
+       # Define o ponto inicial e final
         for i in range(self.height):
             for j in range(self.width):
                 if(self.conteudo[i][j] == 'B'):
                     self.finalState = (i,j)
                 elif(self.conteudo[i][j] == 'A'):
                     self.initialState = (i,j)
-                        
+
     def getInitialState(self):
         return self.initialState
     
@@ -50,23 +59,21 @@ class Frontier():
         if(node not in self.frontier and node.state not in self.exploreSet):
             self.frontier.append(node)
     
-    # Remove e retorna o ultimo elemento a entrar da fronteira, usamos a estrategia de busca em profundidade
-    def removeStack(self):
-        node = self.frontier[-1]
-        self.frontier.pop()
-        self.exploreSet.append(node.state)
-        return node
-    
-    # Remove e retorna o ultimo elemento a entrar na fronteira, usamos a estratégia de busca e largura
-    def removeQueue(self):
-        node = self.frontier[0]
+    # Remove e retorna o ultimo elemento a entrar da fronteira, estamos usando a estrategia
+    # de busca em profundidade
+    def remove(self):
+        node = min(self.frontier, key=lambda x: x.heuristic)
         self.frontier.remove(node)
         self.exploreSet.append(node.state)
         return node
     
     # Cria um nó
     def create_Node(self, state, action, parent): 
-        node = Node(state=state, action=action, parent=parent)
+        steps = 0
+        if(parent != "None"):
+            steps = parent.steps+1
+        node = Node(state=state, action=action, parent=parent, distance=self.heuristic(state), steps=steps)
+        #node.steps = parent.steps+1
         return node
     
     # Define o conjunto de acoes que podem ser feitas a partir de um nó
@@ -83,7 +90,7 @@ class Frontier():
 
         for action, (r,c) in candidates:
             if((r >= 0 and r < self.maze.height and c >= 0 and c < self.maze.width) and (self.maze.conteudo[r][c] == " " or self.maze.conteudo[r][c] == "B")):
-                set_actions.append(Node((r,c),(action,(r,c)),node))
+                set_actions.append(self.create_Node((r,c),(action,(r,c)),node))
 
         return set_actions
  
@@ -91,6 +98,12 @@ class Frontier():
         if(self.maze.finalState == node.state):
             return True
         
+    def heuristic(self, state):
+        heuristic = abs(state[0]-self.maze.finalState[0])+abs(state[1]-self.maze.finalState[1])
+        print(heuristic)
+        return heuristic
+
+
     def empty(self):
         return len(self.frontier) == 0
     
@@ -114,7 +127,7 @@ class main():
     def run_frontier(self):
         while(not self.frontier.empty()):
             # remove um nó da fronteira
-            node = self.frontier.removeQueue()
+            node = self.frontier.remove()
             #verifica se o nó retirado é o estado desejado
             if(self.frontier.goal_Test(node)):
                 self.frontier.path_maze(node)
